130f82026b5905596d580e5d7c29bb0de367aaae
husky <huskyhacks.mk@gmail.com>
2021-02-20 11:20:44 -0800

    fix dependencies, update views to handle GET to lister.html, "front-end" improvements

--- services/web/project/app/views.py
+++ services/web/project/app/views.py
@@ -1,3 +1,4 @@
+import flask
 from flask import current_app as app
 from flask import render_template, request
 from .models import db, Result
@@ -13,48 +14,52 @@ def index():
     return render_template('index.html', title="AgentZero", results=results)
 
 
-@app.route('/listener', methods=['POST'])
+@app.route('/listener', methods=['GET', 'POST'])
 def listener():
-    try:
-        b64content = request.get_data()
-        content = base64.b64decode(b64content).decode("UTF-8")
-        content = json.loads(content)
-    except Exception as e:
-        return str(e), 500
-    if content:
-        result = Result()
-        if 'host' in content:
-            result.host = content['host']
-        if 'domain' in content:
-            result.domain = content['domain']
-        if 'sourcePort' in content:
-            result.sourcePort = content['sourcePort']
-        if 'sourceIP' in content:
-            result.sourceIP = content['sourceIP']
-        if 'user' in content:
-            result.user = content['user']
-        if 'protocol' in content:
-            result.protocol = content['protocol']
-        if 'protocolPort' in content:
-            result.protocolPort = content['protocolPort']
-        if 'ntlmV2Hash' in content:
-            result.ntlmV2Hash = content['ntlmV2Hash']
-        if 'ntlmV1Hash' in content:
-            result.ntlmV1Hash = content['ntlmV1Hash']
-        if 'httpType' in content:
-            result.httpType = content['httpType']
-        if 'httpPort' in content:
-            result.httpPort = content['httpPort']
-        if 'httpSourceIP' in content:
-            result.httpSourceIP = content['httpSourceIP']
-        if 'httpSourcePort' in content:
-            result.httpSourcePort = content['httpSourcePort']
-        if 'clearText' in content:
-            result.clearText = content['clearText']
+    if flask.request.method == 'POST':
         try:
-            db.session.add(result)
-            db.session.commit()
+            b64content = request.get_data()
+            content = base64.b64decode(b64content).decode("UTF-8")
+            content = json.loads(content)
         except Exception as e:
             return str(e), 500
-        return "OK", 200
-    return "MESSAGE: {0}".format(request.is_json)
+        if content:
+            result = Result()
+            if 'host' in content:
+                result.host = content['host']
+            if 'domain' in content:
+                result.domain = content['domain']
+            if 'sourcePort' in content:
+                result.sourcePort = content['sourcePort']
+            if 'sourceIP' in content:
+                result.sourceIP = content['sourceIP']
+            if 'user' in content:
+                result.user = content['user']
+            if 'protocol' in content:
+                result.protocol = content['protocol']
+            if 'protocolPort' in content:
+                result.protocolPort = content['protocolPort']
+            if 'ntlmV2Hash' in content:
+                result.ntlmV2Hash = content['ntlmV2Hash']
+            if 'ntlmV1Hash' in content:
+                result.ntlmV1Hash = content['ntlmV1Hash']
+            if 'httpType' in content:
+                result.httpType = content['httpType']
+            if 'httpPort' in content:
+                result.httpPort = content['httpPort']
+            if 'httpSourceIP' in content:
+                result.httpSourceIP = content['httpSourceIP']
+            if 'httpSourcePort' in content:
+                result.httpSourcePort = content['httpSourcePort']
+            if 'clearText' in content:
+                result.clearText = content['clearText']
+            try:
+                db.session.add(result)
+                db.session.commit()
+            except Exception as e:
+                return str(e), 500
+            return "OK", 200
+        return "MESSAGE: {0}".format(request.is_json)
+
+    else:
+        return render_template('listener.html')
