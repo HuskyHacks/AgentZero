23c4429649673381890960a2ef70df59a5fb6c3e
husky <huskyhacks.mk@gmail.com>
2021-02-21 12:50:36 -0800

    lots of TODOs

--- services/web/project/app/GenerateAgent.py
+++ services/web/project/app/GenerateAgent.py
@@ -4,9 +4,9 @@ import string
 import random
 import zipfile
 import shutil
-from time import sleep
 
 
+# TODO: make sure all of the directories are reletive to the /AgentZero dir location, remove all explicit pathing
 def create_agent(ip, port, inveigh):
     # FORMAT URL FOR FLASK LISTENER
     url = "http://{0}:{1}/listener".format(ip, port)
@@ -24,7 +24,11 @@ def create_agent(ip, port, inveigh):
     cmd2 = "xargs sed -i s|FLASKURL|\"{0}\"|g".format(url)
     p2 = Popen(cmd2.split(), stdin=p1.stdout, stdout=PIPE)
     p2.communicate()
-    # COMPILE WITH MONO (REQUIRES PACKAGE MONO-COMPLETE TO BE INSTALLED)
+
+    # TODO: xbuild is apparently deprecated or soon to be, look into msbuild for this line. Also check dependencies
+    #  for msbuild.
+
+    #  COMPILE WITH MONO (REQUIRES PACKAGE MONO-COMPLETE TO BE INSTALLED)
     cmd = "xbuild /p:Configuration=Release Inveigh.sln"
     p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
     p.communicate()
@@ -39,12 +43,14 @@ def agentNameGenerator(size):
     return agentName
 
 
+# TODO: make all of these reletive paths
 def moveAndRename(agentName):
     shutil.move("/home/husky/Desktop/AgentZero/Inveigh/Inveigh/bin/Release/Inveigh.exe",
                 "/home/husky/Desktop/AgentZero/services/web/project/app/agents/{0}.exe".format(agentName))
     shutil.rmtree('/home/husky/Desktop/AgentZero/Inveigh')
 
 
+# TODO: dynamically set IP address and Inveigh dir
 if __name__ == '__main__':
     create_agent("10.10.1.149", "1776", "/home/husky/Desktop/AgentZero/")
     moveAndRename(agentNameGenerator(8))
